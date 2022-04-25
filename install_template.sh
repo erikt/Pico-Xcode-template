b7c6b2e6737b84c6bd47a9d085b78025872282ea
Erik Tjernlund <erik@tjernlund.net>
2022-04-25 14:10:38 +0200

    Added simple install script to copy template to Xcode template directory

---
+++ install_template.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+TEMPLATE_DIR=$HOME/Library/Developer/Xcode/Templates/Other
+
+mkdir -p $TEMPLATE_DIR
+echo "Copying Raspberry Pi Pico Xcode template to $TEMPLATE_DIR ... \n"
+cp -R "Raspberry Pi Pico.xctemplate" $TEMPLATE_DIR
+
